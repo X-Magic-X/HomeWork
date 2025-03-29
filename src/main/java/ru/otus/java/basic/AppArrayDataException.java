d51f1ff3cf18da0e9b628969278bb2439831f80c
Aleksey <kot543212@mail.ru>
2025-03-29 16:09:59 +0300

    HomeWork8 complete

---
+++ src/main/java/ru/otus/java/basic/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic;
+
+public class AppArrayDataException extends RuntimeException {
+    public AppArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/AppArraySizeException.java
@@ -0,0 +1,6 @@
+package ru.otus.java.basic;
+
+public class AppArraySizeException extends RuntimeException {
+    public AppArraySizeException() {
+    }
+}
