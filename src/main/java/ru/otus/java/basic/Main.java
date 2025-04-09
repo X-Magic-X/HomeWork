2dee5d424090498fdf77e7e7ffcfddf0e3d51d69
Aleksey <kot543212@mail.ru>
2025-04-10 00:44:26 +0300

    HomeWork10 complete

--- src/main/java/ru/otus/java/basic/Main.java
+++ src/main/java/ru/otus/java/basic/Main.java
@@ -2,6 +2,15 @@ package ru.otus.java.basic;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Barabanov", "+7305235253");
+        phoneBook.add("Barabanov", "84832483284");
+        phoneBook.add("Кузьмин Евгений Анатольевич", "+7986539487");
+
+        System.out.println(phoneBook.toString());
+        System.out.println(phoneBook.find("Barabanov"));
+        System.out.println(phoneBook.containsPhoneNumber("84832483284"));
+        System.out.println(phoneBook.containsPhoneNumber("+798539487"));
+        System.out.println(phoneBook.find("Кузьмин Евгений Анатольевич"));
     }
 }
