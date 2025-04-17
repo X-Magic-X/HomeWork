1d708389e940609be409d89c24ea5634499268a5
Aleksey <kot543212@mail.ru>
2025-04-17 19:04:02 +0300

    HomeWork11 complete

--- src/main/java/ru/otus/java/basic/Main.java
+++ src/main/java/ru/otus/java/basic/Main.java
@@ -1,7 +1,21 @@
 package ru.otus.java.basic;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Alex", Position.DIRECTOR, 1L);
+        Person person2 = new Person("Jack", Position.DRIVER, 2L);
+
+        PersonDataBase base = new PersonDataBase();
+        base.add(person);
+        base.add(person2);
+        System.out.println(base.findByld(2L));
+        System.out.println(base.isManager(person2));
+        System.out.println(base.isEmployee(2L));
+
+        int[] arr = {1, 2, 52, 4, 47, 6, 152, 8};
+        ArrayTool.bubbleSort(arr);
+        for (int i : arr)
+            System.out.println(i);
     }
 }
