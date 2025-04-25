83af9c3ef8c0f10e5051bcedd107b5b601f6e2ab
Aleksey <kot543212@mail.ru>
2025-04-25 12:24:26 +0300

    The code is divided into methods

--- src/main/java/ru/otus/java/basic/Main.java
+++ src/main/java/ru/otus/java/basic/Main.java
@@ -8,16 +8,27 @@ public class Main {
     public static void main(String[] args) {
         final String PATH = "src/main/java/ru/otus/java/basic/";
         Scanner scanner = new Scanner(System.in);
-        File fileDirectory = new File(PATH);
+        showAllTxtFileByPath(PATH);
+        String filePath = selectAndReadFile(PATH);
+        String data = scanner.next();
+        writeToFile(filePath, data);
+    }
+
+    static void showAllTxtFileByPath(String path) {
+        File fileDirectory = new File(path);
         File[] listFiles = fileDirectory.listFiles();
         for (File file : listFiles) {
             if (file.getAbsolutePath().endsWith(".txt")) {
                 System.out.println(file);
             }
         }
+    }
+
+    static String selectAndReadFile(String path) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите имя файла с которым хотите работать: ");
-        String name = scanner.next();
-        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(PATH + name))) {
+        String filePath = path + scanner.next();
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(filePath))) {
             int n = in.read();
             while (n != -1) {
                 System.out.print((char) n);
@@ -27,11 +38,13 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String data = scanner.next();
-        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(PATH + name))) {
+        return filePath;
+    }
+
+    static void writeToFile(String path, String data) {
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(path))) {
             byte[] buffer = data.getBytes(StandardCharsets.UTF_8);
             for (byte b : buffer) out.write(b);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
