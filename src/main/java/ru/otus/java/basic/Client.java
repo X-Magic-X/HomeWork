3a3852e8a47377acfd328bcdcbbb99ee2a5033bc
Aleksey <kot543212@mail.ru>
2025-07-19 15:32:06 +0300

    HomeWork complete

--- src/main/java/ru/otus/java/basic/Client.java
+++ src/main/java/ru/otus/java/basic/Client.java
@@ -1,23 +1,39 @@
 package ru.otus.java.basic;
 
-import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String[] userMessage = new String[2];
-            try (Socket socket = new Socket("localhost", 8091)) {
-                System.out.println("Client APP STARTED!");
-                ClientTool clientTool = new ClientTool(socket.getInputStream(),
-                        socket.getOutputStream());
-                userMessage[0] = scanner.nextLine();
+
+        try (Socket socket = new Socket("localhost", 8091)) {
+            System.out.println("Client APP STARTED!");
+            ClientTool clientTool = new ClientTool(socket.getInputStream(),
+                    socket.getOutputStream());
+            Thread readerThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        clientTool.read();
+                    }
+                } catch (Exception e) {
+                    System.out.println("Соединение с сервером прервано");
+                }
+            });
+            readerThread.setDaemon(true);
+            readerThread.start();
+            while (true) {
+                String userMessage = scanner.nextLine();
+                if (userMessage.equalsIgnoreCase("exit")) {
+                    clientTool.send(userMessage);
+                    readerThread.stop();
+                    clientTool.close();
+                    break;
+                }
                 clientTool.send(userMessage);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/ru/otus/java/basic/ClientTool.java
+++ src/main/java/ru/otus/java/basic/ClientTool.java
@@ -3,22 +3,25 @@ package ru.otus.java.basic;
 import java.io.*;
 
 public class ClientTool implements AutoCloseable {
-    private final ObjectInputStream inputStream;
-    private final ObjectOutputStream outputStream;
+    private final DataInputStream inputStream;
+    private final DataOutputStream outputStream;
 
     public ClientTool(InputStream inputStream, OutputStream outputStream) throws IOException {
-        this.inputStream = new ObjectInputStream(inputStream);
-        this.outputStream = new ObjectOutputStream(outputStream);
+        this.inputStream = new DataInputStream(inputStream);
+        this.outputStream = new DataOutputStream(outputStream);
     }
 
-    public void send(String[] message) throws IOException {
-        outputStream.writeObject(message);
+    public void send(String message) throws IOException {
+        outputStream.writeUTF(message);
         outputStream.flush();
+    }
+
+    public void read() {
         try {
             String result = inputStream.readUTF();
             System.out.println(result);
         } catch (IOException e) {
-            System.out.println("Сервер закрыл соединение");
+            System.out.println("Сервер закрыл соединение.");
         }
     }
 
