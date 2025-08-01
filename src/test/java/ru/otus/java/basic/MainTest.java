5914477037201fc12bacfcdd1973a54f9eb3858f
Aleksey <kot543212@mail.ru>
2025-08-02 01:09:18 +0300

    reworked checkOfOneTwo

--- src/test/java/ru/otus/java/basic/MainTest.java
+++ src/test/java/ru/otus/java/basic/MainTest.java
@@ -1,21 +1,33 @@
 package ru.otus.java.basic;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 class MainTest {
 
     int[] arr1 = new int[]{1, 2, 2, 3, 1, 4, 1, 2, 1};
     int[] arr2 = new int[]{2, 4, 3, 2, 4, 5};
-    int[] arr3 = new int[]{1, 1, 1, 2, 2, 1};
-    int[] arr4 = new int[]{1, 1, 1, 1};
+
+    static Stream<Arguments> checkOfOneTwo() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 2, 3, 1, 4, 1, 2, 1}, false),
+                Arguments.of(new int[]{2, 4, 3, 2, 4, 5}, false),
+                Arguments.of(new int[]{1, 1, 1, 2, 2, 1}, true),
+                Arguments.of(new int[]{1, 1, 1, 1}, false)
+        );
+    }
 
     @Test
     void nextOfOneOk() {
         List<Integer> actual = Main.nextOfOne(arr1);
         List<Integer> expected = new ArrayList<>(List.of(2, 4, 2));
+
         Assertions.assertEquals(actual.size(), expected.size());
         for (int i = 0; i < actual.size(); i++) {
             Assertions.assertEquals(actual.get(i), expected.get(i));
@@ -29,13 +41,11 @@ class MainTest {
         });
     }
 
-    @Test
-    void checkOfOneTwo() {
-        boolean result1 = Main.checkOfOneTwo(arr1);
-        boolean result2 = Main.checkOfOneTwo(arr3);
-        boolean result3 = Main.checkOfOneTwo(arr4);
-        Assertions.assertFalse(result1);
-        Assertions.assertTrue(result2);
-        Assertions.assertFalse(result3);
+    @ParameterizedTest
+    @MethodSource("checkOfOneTwo")
+    void checkOfOneTwo(int[] numbers, boolean expected) {
+        boolean actual = Main.checkOfOneTwo(numbers);
+
+        Assertions.assertEquals(actual, expected);
     }
 }
